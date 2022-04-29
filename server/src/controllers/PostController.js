9fab976603602dfdcf1290c1af42a4f318e4a28d
Manuel Angel Berger Molina <manuel.molina@tecadi.com.br>
2022-04-29 20:31:33 -0300

    corrigindo arquivos server

--- server/src/controllers/PostController.js
+++ server/src/controllers/PostController.js
@@ -1,43 +1,40 @@
-const Post = require('../models/Pots')
-const sharp = require('sharp')
-const path = require('path')
-const fs = require('fs')
+const Post = require("../models/Posts");
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs");
 
 module.exports = {
   async index(req, res) {
-    const posts = await Post.find().sort('-createdAt')
-    return res.json(posts)
+    const posts = await Post.find().sort("-createdAt");
+    return res.json(posts);
   },
   async store(req, res) {
     //selecionando dados enviados pelo usuário
-    const data = req.body
-    const { filename: image } = req.file
+    const data = req.body;
+    const { filename: image } = req.file;
 
     //transformando imagem em jpg
-    const [name] = image.split('.')
-    const fileName = `${name}.jpg`
+    const [name] = image.split(".");
+    const fileName = `${name}.jpg`;
 
     //redimensionando a imagem
     await sharp(req.file.path)
       .resize(500)
       .jpeg({ quality: 70 })
-      .toFile(
-        path.resolve(req.file.destination, 'resized', fileName)
-      )
+      .toFile(path.resolve(req.file.destination, "resized", fileName));
 
     //apagando a imagem original
-    fs.unlinkSync(req.file.path)
+    fs.unlinkSync(req.file.path);
 
     //criando post
     const post = await Post.create({
       ...data,
-      fileName
-    })
+      fileName,
+    });
 
     //emitindo post criado a todos os usuários conectados
-    req.io.emit('post', post)
+    req.io.emit("post", post);
 
-
-    return res.json(post)
-  }
-}
+    return res.json(post);
+  },
+};
